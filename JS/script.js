3a788c6e26db2d984a5af55756c92b8caaec6f94
SkShoheb33 <shaikshoheb9k@gmail.com>
2023-03-02 14:53:58 +0530

    version 1.2.1

--- JS/script.js
+++ JS/script.js
@@ -3,13 +3,22 @@ sound = new Audio();
 sound.src = "JS\\Assets\\100countSound.mp3";
 incrSound = new Audio();
 incrSound.src = "JS\\Assets\\incrSoundOriginal.mp3"
+displayTime = () => {
+
+    let date = new Date();
+    document.getElementById("time").innerHTML = date.toLocaleString();
+}
+setInterval(
+    displayTime,
+    1000
+)
 incriment = () => {
     count += 1;
-    document.getElementById("count").innerHTML = count;
     if (count % 100 === 0)
         sound.play();
     else
         incrSound.play();
+    document.getElementById("count").innerHTML = count;
 }
 decriment = () => {
     if (count > 0) {
@@ -18,8 +27,19 @@ decriment = () => {
     }
 }
 reset = () => {
-    if (count > 0)
-        window.alert("Do you want to reset");
     count = 0;
     document.getElementById("count").innerHTML = count;
+}
+
+closeMenu = () => {
+    let menu = document.getElementById("menu");
+    menu.style.display = "none";
+    let open = document.getElementById("open");
+    open.style.display = "block";
+}
+openMenu = () => {
+    let menu = document.getElementById("menu");
+    let open = document.getElementById("open");
+    open.style.display = "none";
+    menu.style.display = "flex";
 }
