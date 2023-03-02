93f57f1c6046fbf2f897ec0d11662688783251ea
SkShoheb33 <shaikshoheb9k@gmail.com>
2023-03-02 12:01:36 +0530

    version 1.2.0

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,15 @@
 let count = parseInt(document.getElementById("count").innerHTML);
+sound = new Audio();
+sound.src = "JS\\Assets\\100countSound.mp3";
+incrSound = new Audio();
+incrSound.src = "JS\\Assets\\incrSoundOriginal.mp3"
 incriment = () => {
     count += 1;
     document.getElementById("count").innerHTML = count;
+    if (count % 100 === 0)
+        sound.play();
+    else
+        incrSound.play();
 }
 decriment = () => {
     if (count > 0) {
